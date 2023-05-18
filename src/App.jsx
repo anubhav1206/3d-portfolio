060857b5ce53767286ea66f30f79f04163265504
SailingMonkey <bennyrosa.19@gmail.com>
2023-05-18 16:58:13 -0400

    Simple red cube and added components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+
+function CarShow() {
+  return (
+    <>
+      <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
+      <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
+
+      <mesh>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshBasicMaterial color={"red"} />
+      </mesh>
+
+    </>
+  );
+}
+
 const App= () => {
   return (
-      <div>
-        Test
-      </div>
+      <Suspense fallback={null}>
+        <Canvas shadows>
+          <CarShow />
+        </Canvas>
+      </Suspense>
   )
 }
 
