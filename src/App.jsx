0855ba32a5c2558d9126e88fb6fa1cafbdb542d6
SailingMonkey <80559254+SailingMonkey@users.noreply.github.com>
2023-08-14 17:12:32 -0400

    Added some comments to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ import { Terrain, Car, Grid } from "./components"
 function ShowScene() {
   return (
     <>
+      {/*The cameras movement is limited so that it doesn't clip through the floor or the mountains, nor that the user is able to look from above and disabled zoom as this can also clip through things*/}
       <OrbitControls target={[0, 0, 0]} minPolarAngle={Math.PI / 2.3} maxPolarAngle={Math.PI / 2.1} minAzimuthAngle={-Math.PI / 19} maxAzimuthAngle={Math.PI / 19} enableZoom={false}/>
+      {/*The camera position may need to change, but this will depend on the car used and the "billboards" that get added*/}
       <PerspectiveCamera makeDefault fov={50} position={[0, 0.06, 1]}/>
 
       {/*This changes the color of the background*/}
@@ -22,7 +24,7 @@ function ShowScene() {
         castShadow
         shadow-bias={-0.0001}
       />*/}
-      {/*Keeping cube for now to have a refference to 0,0,0 coordinates */}
+      {/*Uncomment if need cube at the 0,0,0 coordinates for position and size reference*/}
       {/*<mesh>
         <boxGeometry args={[1, 1, 1]} />
         <meshBasicMaterial color={"red"} />
@@ -36,6 +38,7 @@ const App= () => {
   return (
       <Suspense fallback={null}>
         <Canvas shadows>
+          {/*Adds fog at the end of the grid, will probably need to change color later to match that of the Sun at the end of the track*/}
           <fog attach="fog" color="black" near={1} far={2.5} />
           <ShowScene />
         </Canvas>
